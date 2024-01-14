c49cb28a63da785dda5738fd659417989b1e5576
Dawson Toth <dawsont@ifit.com>
2024-01-14 00:44:22 -0500

    feat: Make it say stuff

---
+++ README.md
@@ -0,0 +1,86 @@
+
+main
+wide
+left
+right
+guitar
+drums
+lower thirds fill
+tripod
+flycam
+piano
+floor
+video wall main
+handheld
+audience
+video
+lower thirds key
+confidence
+video wall columns
+imag rig desktop
+hyperdeck
+
+cut main
+cut wide
+cut left
+cut right
+cut guitar
+cut drums
+cut lower thirds fill
+cut tripod
+cut flycam
+cut piano
+cut floor
+cut video wall main
+cut handheld
+cut audience
+cut video
+cut lower thirds key
+cut confidence
+cut video wall columns
+cut imag rig desktop
+cut hyperdeck
+
+
+fading main
+fading wide
+fading left
+fading right
+fading guitar
+fading drums
+fading lower thirds fill
+fading tripod
+fading flycam
+fading piano
+fading floor
+fading video wall main
+fading handheld
+fading audience
+fading video
+fading lower thirds key
+fading confidence
+fading video wall columns
+fading imag rig desktop
+fading hyperdeck
+
+
+faded main
+faded wide
+faded left
+faded right
+faded guitar
+faded drums
+faded lower thirds fill
+faded tripod
+faded flycam
+faded piano
+faded floor
+faded video wall main
+faded handheld
+faded audience
+faded video
+faded lower thirds key
+faded confidence
+faded video wall columns
+faded imag rig desktop
+faded hyperdeck
---
+++ index.js
@@ -0,0 +1,99 @@
+const playSound = require('play-sound');
+const { Atem } = require('atem-connection');
+
+process.on('SIGINT', cleanUp);
+const audios = [];
+const player = playSound(opts = {});
+const myAtem = new Atem();
+const inputs = ['main', 'wide', 'left', 'right', 'guitar', 'drums', 'lower thirds fill', 'tripod', 'flycam', 'piano', 'floor', 'video wall main', 'handheld', 'audience', 'video', 'lower thirds key', 'confidence', 'video wall columns', 'desktop', 'hyperdeck'];
+const superSourceName = 'SS1';
+const atemIP = '10.1.34.34';
+// const superSourceName = 'MP2';
+// const atemIP = '127.0.0.1';
+
+(async () => {
+
+  myAtem.on('stateChanged', async (state, pathToChange) => {
+    console.log('stateChanged', pathToChange.sort());
+    // TODO: Handle lower thirds.
+    const previewName = inputs[state.video.mixEffects[0].previewInput - 1];
+    const programName = inputs[state.video.mixEffects[0].programInput - 1];
+    if (pathToChange.includes('video.mixEffects.0.transitionPosition')) {
+      if (pathToChange.includes('video.mixEffects.0.programInput')) {
+        return play(`faded ${programName}`);
+      }
+      if (pathToChange.includes('video.mixEffects.0.previewInput')) {
+        return play(`fading ${previewName}`);
+      }
+      return;
+    }
+    if (pathToChange.includes('video.mixEffects.0.programInput')) {
+      return play(`cut ${programName}`);
+    }
+    if (pathToChange.includes('video.mixEffects.0.previewInput')) {
+      return play(previewName);
+    }
+  });
+  myAtem.on('receivedCommand', (command) => {
+    console.log('receivedCommand', command);
+  });
+
+  myAtem.on('connected', async () => {
+    await sleep(2000);
+
+    const superSource = Object.values(myAtem.state.inputs).find(input => input.shortName === superSourceName);
+    if (superSource) {
+      inputs[superSource.inputId - 1] = 'supersource';
+      console.log(`SuperSource found at ${superSource.inputId}`);
+    }
+    else {
+      console.log(`SuperSource not found with shortName ${superSourceName}!`);
+    }
+
+    // await sleep(2000);
+    // await myAtem.changePreviewInput(15);
+    // await sleep(2000);
+    // await myAtem.autoTransition();
+    // await sleep(6000);
+    // await myAtem.changePreviewInput(1);
+    // await sleep(2000);
+    // await myAtem.cut();
+    // await sleep(2000);
+    // await myAtem.changeProgramInput(15);
+    // await sleep(2000);
+    // for (let i = 1; i <= 20; i++) {
+    //   await myAtem.changePreviewInput(i);
+    //   await sleep(2000);
+    //   await myAtem.cut();
+    //   await sleep(2000);
+    // }
+    // await sleep(2000);
+    // await myAtem.cut();
+    // await sleep(2000);
+    // await myAtem.setUpstreamKeyerOnAir(true, 1, 1);
+    // await sleep(2000);
+    // await myAtem.setUpstreamKeyerOnAir(false, 1, 1);
+    // await sleep(2000);
+    // await myAtem.setUpstreamKeyerOnAir(true, 1, 1);
+  });
+
+  await myAtem.connect(atemIP);
+})();
+
+function sleep(ms = 1000) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+}
+
+function play(name) {
+  audios.push(player.play(`audio/${name}.m4a`));
+}
+
+async function cleanUp() {
+  console.log('');
+  console.log('Exiting...');
+  audios?.forEach(audio => audio.kill());
+  myAtem.disconnect().then();
+  console.log('Done!');
+  console.log('');
+  process.exit();
+}
