41962393e73e4da071113d544ce30c7f7bf3165a
Dawson Toth <dawsont@ifit.com>
2025-05-24 14:41:24 -0400

    chore: Remove unused md5

--- src/utils/md5.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-export function generateMD5Hash(input: string): string {
-  const hash = crypto.createHash('md5');
-  hash.update(input);
-  return hash.digest('hex');
-}
